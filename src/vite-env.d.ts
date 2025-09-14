3c17547c46858afe1ff8713a6fe56dacc5b16c1f
sulbin <tlsdnr1135@naver.com>
2025-09-15 01:56:41 +0900

    axios 추가.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,5 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
 interface ImportMetaEnv {
   readonly BASE_URL: string
   // 여기에 다른 환경 변수들도 추가할 수 있습니다
